450bcdc49ed379aa786f7bd6129a7eb93fa9b9d0
DanielMichaelEdwards <edwardsdanielmichael@gmail.com>
2018-03-13 19:03:25 +0300

    Handling server codes in found_terminator

--- ftpClient.py
+++ ftpClient.py
@@ -13,10 +13,12 @@ class controlConnection(asynchat.async_chat):
         print("Initializing")
         self.set_terminator("\n")
         self.data=""
+        self.password = "123"
 
         # connect to ftp server
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((host, 21))
+        self.last_command = None
 
     def handle_connect(self):
         #connection succeeded
@@ -34,17 +36,39 @@ class controlConnection(asynchat.async_chat):
             data = data[:-1]
         self.data = ""
 
-        print("S:" + str(data))
+        if not re.match("\d\d\d ", data):
+            return
+        #this was the last line in the response, check if special treatment is needed
 
-        if re.match("\d\d\d ", data):
-            #this was the last line in this response, send the next command to the server
-            try:
-                command = self.commands.pop(0)
-            except IndexError:
-                pass #no more commands
+        if self.last_command == None:
+            #handle connection
+            if data.startswith("220"):
+                self.last_command = "USER"
+                self.push("USER anonymous\r\n")
+                return
             else:
-                print("C:", command)
-                self.push(command + "\r\n")
+                raise Exception("ftp login failed")
+        elif self.last_command == "USER":
+            #handle user response
+            if data.startswith("230"):
+                pass #user accepted
+            elif data.startswith("331") or data.startswith("333"):
+                self.last_command = "PASS"
+                self.push("PASS " +self.password + "\r\n")
+                return
+            else:
+                raise Exception("ftp login failed")
+        elif self.last_command == "PASS":
+            if data.startswith("230"):
+                pass
+            else:
+                raise Exception("ftp login failed")
+
+        #send the next command to the server
+        try:
+            self.push(self.conmmands.pop(0) + "\r\n")
+        except IndexError:
+            pass #no more commands
 
 controlConnection("ftp.python.org")
 asyncore.loop()
