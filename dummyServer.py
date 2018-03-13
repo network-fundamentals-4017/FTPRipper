f7e197e8714df83613f97dadc6eda3c3b39c94a0
DanielMichaelEdwards <edwardsdanielmichael@gmail.com>
2018-03-13 13:10:57 +0300

    Dummy server

---
+++ dummyServer.py
@@ -0,0 +1,13 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+authorizer = DummyAuthorizer()
+authorizer.add_user("user", "12345", "/Users/Daniel", perm="elradfmw")
+authorizer.add_anonymous("/Users/Daniel", perm="elradfmw")
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+server = FTPServer(("127.0.0.1", 1026), handler)
+server.serve_forever()
