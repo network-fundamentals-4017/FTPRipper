fa2b3e8f5371fbc5433c4f4a96bbf143b8a7ff91
DanielMichaelEdwards <edwardsdanielmichael@gmail.com>
2018-03-19 16:31:41 +0300

    Added LIST command to server

--- ftpServer.py
+++ ftpServer.py
@@ -1,4 +1,4 @@
-import socket
+import socket, os
 
 def Main():
     print(socket.gethostname())
@@ -10,8 +10,7 @@ def Main():
     controlSocket.listen(1)
 
     controlConnection, controlAddress = controlSocket.accept()
-    requestUser(controlConnection)
-
+    USER(controlConnection)
     while True:
         data = controlConnection.recv(1024).decode()
         if not data:
@@ -20,27 +19,28 @@ def Main():
         command = getCommnad(data)
         info = getInfo(data)
         if command=="PASV":
-            dataSocket = setupDataSocket(controlConnection)
-
+            dataSocket = PASV(controlConnection)
+        elif command=="LIST":
+            LIST(controlConnection, dataSocket)
 
 def createControlSocket(host, port):
     controlSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     controlSocket.bind((host, port))
     return controlSocket
 
-def requestUser(controlConnection):
+def USER(controlConnection):
     message = "220 Server socket established"
     controlConnection.send(message.encode())
     print(message)
     data = controlConnection.recv(1024).decode()
     username = data[5:].strip()
     if checkUserExists(username):
-        requestPassword(controlConnection, username)
+        PASS(controlConnection, username)
     else:
         message = "530 Unknown user"
         controlConnection.send(message.encode())
 
-def requestPassword(controlConnection, username):
+def PASS(controlConnection, username):
     message = "331 Password required"
     controlConnection.send(message.encode())
     data = controlConnection.recv(1024).decode()
@@ -65,19 +65,39 @@ def getInfo(data):
     info = data[data.find(' ')+1:]
     return info
 
-def setupDataSocket(controlConnection):
+def PASV(controlConnection):
     address = socket.gethostbyname(socket.gethostname())
     address = address.split(".")
     address = ','.join(address)
     address = "("+address+",30,60)"
     message = "227 Passive Mode " + address
-
-    dataSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    dataSocket.bind((socket.gethostbyname(socket.gethostname()), 7740))
-    dataSocket.listen(1)
+    dataSocket = getDataSocket(socket.gethostbyname(socket.gethostname()), 7740)
     controlConnection.send(message.encode())
     print("Passive data connection set up")
     return dataSocket
+
+def getDataSocket(host, port):
+    dataSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    dataSocket.bind((host, port))
+    dataSocket.listen(1)
+    return dataSocket
+
+def LIST(controlConnection, dataSocket):
+    message = "150 Here comes the directory listing..."
+    controlConnection.send(message.encode())
+    dataConnection, address = dataSocket.accept()
+    listing = ""
+    # detect the current working directory
+    path = os.getcwd()
+
+    # read the entries
+    with os.scandir(path) as listOfEntries:
+        for entry in listOfEntries:
+            # print all entries that are files
+            if entry.is_file():
+                listing = listing + '\t' + entry.name
+    print(listing)
+    dataConnection.send(listing.encode())
 
 if __name__ == '__main__':
     Main()
